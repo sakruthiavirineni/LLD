315235cd0fe5ae288afbf6a761a6b10e0dfc1129
Sakruthi Avirineni <86926099+sakruthiavirineni@users.noreply.github.com>
2023-10-22 14:02:59 -0700

    Create snakeladder.cpp

---
+++ SnakeLadder/snakeladder.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class snakesLadder
+{
+public:
+
+  int start;
+  int end;
+
+    snakesLadder (int start, int end)
+  {
+    this->start = start;
+    this->end = end;
+  }
+};
